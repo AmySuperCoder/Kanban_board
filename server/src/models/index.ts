45fb7775ea07cd10233af5190ab4333017187ba3
AmySuperCoder <tribegriffith@gmail.com>
2025-01-06 13:36:32 -0500

    fixes for deploying

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,27 +6,27 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-// const sequelize = process.env.DB_URL
-//   ? new Sequelize(process.env.DB_URL)
-//   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-//       host: 'localhost',
-//       dialect: 'postgres',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//     });
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PW as string,
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-  },
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME as string,
+//   process.env.DB_USER as string,
+//   process.env.DB_PW as string,
+//   {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//   },
+// );
 
 export default sequelize;
 
