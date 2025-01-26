4c5f5902b794c2086749bff627fe4db76221ea66
AmySuperCoder <tribegriffith@gmail.com>
2025-01-26 17:29:04 -0500

    update gitignore

--- server/dist/models/index.js
+++ server/dist/models/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
+console.log(process.env);
 const sequelize = process.env.DB_URL
     ? new Sequelize(process.env.DB_URL)
     : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
@@ -12,6 +13,16 @@ const sequelize = process.env.DB_URL
             decimalNumbers: true,
         },
     });
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME as string,
+//   process.env.DB_USER as string,
+//   process.env.DB_PW as string,
+//   {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//   },
+// );
+export default sequelize;
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
